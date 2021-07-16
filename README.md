a1b5ebb4766c76ea76e596fc6fb909c55db4bd01
Michael Snider <msilvprog7@gmail.com>
2021-07-16 07:13:37 -0700

    Merge prototype

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-msilvprog7.github.io
-====================
+# msilvprog7.github.io
 
-My personal website.
+The following is a site for my resume, which includes
+my experience, education, skills, and projects.
+
+The site is composed of:
+
+- [Typescript React App](https://create-react-app.dev/docs/adding-typescript/)
+- [React Bootstrap](https://react-bootstrap.github.io/getting-started/introduction/)
+
+## Setup
+
+Install:
+
+- [Node.JS and NPM](https://nodejs.org/en/)
+- [Yarn](https://yarnpkg.com/)
+
+From command line, run:
+
+```cmd
+yarn install
+```
+
+## Develop
+
+From command line, run:
+
+```cmd
+yarn start
+```
+
+## Deploy
+
+From command line, run:
+
+```cmd
+yarn deploy
+```
---
+++ src/components/Files.tsx
@@ -0,0 +1,29 @@
+import path from "path";
+
+const DirectoriesImages = "images";
+const DirectoriesImagesProjects = path.join(DirectoriesImages, "projects");
+const Directories = {
+  Images: DirectoriesImages,
+  Projects: DirectoriesImagesProjects,
+};
+
+function prependPublicUrl(...paths: string[]) {
+  paths.unshift(process.env.PUBLIC_URL);
+  return path.join(...paths);
+}
+
+export default Object.assign(
+  {},
+  {
+    Resume: prependPublicUrl("resume.pdf"),
+    Images: {
+      Profile: prependPublicUrl(Directories.Images, "profile.jpg"),
+      Projects: {
+        Boulder: prependPublicUrl(Directories.Projects, "boulder.png"),
+        Capstone: prependPublicUrl(Directories.Projects, "capstone.png"),
+        NDex: prependPublicUrl(Directories.Projects, "n-dex.png"),
+        Tsp: prependPublicUrl(Directories.Projects, "tsp.jpg"),
+      },
+    },
+  }
+);
